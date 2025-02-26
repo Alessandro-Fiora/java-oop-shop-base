f19ed51949bded1839f6c548145f5a817561ea49
Alessandro-Fiora <fiora.ale97@gmail.com>
2025-02-26 13:17:06 +0100

    fix: rounded output to two decimals

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -2,15 +2,15 @@ package org.lessons.java.shop;
 
 public class Main {
 public static void main(String[] args) {
-    Prodotto prodotto1 = new Prodotto("Scarpa Instinct VSR", "Scarpette da arrampicata", 130);
+    Prodotto prodotto1 = new Prodotto("Scarpa Instinct VSR", "Scarpette da arrampicata", 130.5);
     Prodotto prodotto2 = new Prodotto("Petzl Quark Martello", "Piccozza da alpinismo tecnico e ghiaccio", 180);
 
     System.out.println(prodotto1.getFullName());
-    System.out.println(prodotto1.getBasePrice());
-    System.out.println(prodotto1.getFullPrice());
+    System.out.println(String.format("%.2f", prodotto1.getBasePrice()));
+    System.out.println(String.format("%.2f", prodotto1.getFullPrice()));
 
     System.out.println(prodotto2.getFullName());
-    System.out.println(prodotto2.getBasePrice());
-    System.out.println(prodotto2.getFullPrice());
+    System.out.println(String.format("%.2f", prodotto2.getBasePrice()));
+    System.out.println(String.format("%.2f", prodotto2.getFullPrice()));
 }
 }
